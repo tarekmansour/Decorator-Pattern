024928db4c46b4fe6566c99eb44cf44d1a2e73ea
tarekmansour <tarekmansour.net@gmail.com>
2023-08-25 17:10:11 +0200

    implement decorator patter to apply caching strategy

---
+++ ProductsApiClient/CachedProductClient.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using ProductsApiClient.Models;
+using System.Xml.Linq;
+
+namespace ProductsApiClient;
+
+public class CachedProductClient : IProductClient
+{
+    private readonly IProductClient _decorated;
+    private readonly IMemoryCache _memoryCache;
+
+    public CachedProductClient(IProductClient decorated, IMemoryCache memoryCache)
+    {
+        _decorated = decorated;
+        _memoryCache = memoryCache;
+    }
+
+    public async Task<Product?> GetByIdAsync(Guid Id, CancellationToken cancellationToken = default)
+    {
+        string cacheKey = $"products-{Id}";
+
+        return await _memoryCache.GetOrCreateAsync(
+            cacheKey,
+            entry =>
+            {
+                entry.SetAbsoluteExpiration(TimeSpan.FromMinutes(30));
+                return _decorated.GetByIdAsync(Id, cancellationToken);
+            });
+    }
+}
--- ProductsApiClient/DependencyInjection/ServicesExtensions.cs
+++ ProductsApiClient/DependencyInjection/ServicesExtensions.cs
@@ -4,7 +4,7 @@ namespace ProductsApiClient.DependencyInjection;
 
 public static class ServicesExtensions
 {
-    public static IHttpClientBuilder AddDataClient(
+    public static IHttpClientBuilder AddProductsClient(
         this IServiceCollection services,
         Uri baseAddress)
     {
@@ -12,10 +12,30 @@ public static class ServicesExtensions
         {
             throw new ArgumentNullException(
                 nameof(services),
-                $"Failed to configure services: IServiceCollection is null. Parameter name: {nameof(services)}");
+                "Failed to configure services: IServiceCollection is null.");
         }
 
         return services.AddHttpClient<IProductClient>()
             .ConfigureHttpClient(client => client.BaseAddress = baseAddress);
+    }
+
+    public static IHttpClientBuilder AddCachedProductsClient(
+        this IServiceCollection services,
+        Uri baseAddress)
+    {
+        if (services is null)
+        {
+            throw new ArgumentNullException(
+                nameof(services),
+                "Failed to configure services: IServiceCollection is null.");
+        }
+
+        var httpClientBuilder = services.AddHttpClient<IProductClient>()
+            .ConfigureHttpClient(client => client.BaseAddress = baseAddress);
+
+        services.Decorate<IProductClient, CachedProductClient>();
+        services.AddMemoryCache();
+
+        return httpClientBuilder;
     }
 }
